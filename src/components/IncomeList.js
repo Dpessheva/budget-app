44369dfc4bab83415fefbc1a4286095b813154e7
Dpessheva <dpessheva@gmail.com>
2020-04-16 16:19:03 +0200

    first commit

---
+++ src/components/IncomeList.js
@@ -0,0 +1,18 @@
+import React, {useContext} from 'react';
+import {GlobalContext} from '../contexts/GlobulState';
+import IncomeTransaction from '../components/IncomeTransaction';
+const IncomeList = () => {
+    const { incomeTransactions } = useContext(GlobalContext);
+    return (
+        <div className="transactions transactions-income">
+            <h2>Transaction History</h2>
+            <ul className="transaction-list">
+                {incomeTransactions.map((incomeTransaction) => (
+                    <IncomeTransaction key={incomeTransaction.id} incomeTransaction={incomeTransaction} />
+                ))}
+            </ul>
+        </div>
+    )
+};
+
+export default IncomeList;
