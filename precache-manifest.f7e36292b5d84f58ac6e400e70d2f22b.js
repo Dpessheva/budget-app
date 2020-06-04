b6f76f75cb1795cd73d53133c9b8ef311580968b
Dpessheva <dpessheva@gmail.com>
2020-06-04 19:33:19 +0200

    Updates

---
+++ precache-manifest.f7e36292b5d84f58ac6e400e70d2f22b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a34e0a34de9d3a136625138c838c0d20",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "3cdf2c20a0d6626e48e1",
+    "url": "/budget-app/static/css/main.a5f4379b.chunk.css"
+  },
+  {
+    "revision": "ddf7c6a4136c246fdcca",
+    "url": "/budget-app/static/js/2.a6cbcd05.chunk.js"
+  },
+  {
+    "revision": "77e7f328dacc981fca681ee4ae94d669",
+    "url": "/budget-app/static/js/2.a6cbcd05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cdf2c20a0d6626e48e1",
+    "url": "/budget-app/static/js/main.99708753.chunk.js"
+  },
+  {
+    "revision": "665615af624d925aca6f",
+    "url": "/budget-app/static/js/runtime-main.ab54b60e.js"
+  }
+]);
